80b323b2bfed3a435980343a11b5f32aa0742b3e
Florian Didron <florian@111studio.jp>
2016-08-13 21:17:29 +0900

    Logo fadein

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,11 +2,23 @@ import { h, Component } from 'preact';
 import style from './style';
 
 export default class Home extends Component {
-  render() {
+
+  constructor() {
+    super();
+    this.state = {
+      logoStyle: style.hidden
+    };
+  }
+
+  componentDidMount() {
+    setTimeout( () => this.setState({logoStyle: style.revealed}), 1 );
+  }
+
+  render(props, state) {
     return (
       <div class={ style.home }>
         <div class={ style.topHero }>
-          <img src={ require('../../assets/logo.png') } alt="Angels Inc Logo"/>
+          <img class={ state.logoStyle } src={ require('../../assets/logo.png') } alt="Angels Inc Logo"/>
         </div>
         <div class={ style.bottomHero }>
           <h1>COMING SOON</h1>
